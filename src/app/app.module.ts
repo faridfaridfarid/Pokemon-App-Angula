b7c53c73fa44c6bd605061bfc610310f9f5a934d
FaridBou <farid-bouchikhi@hotmail.fr>
2018-08-12 23:15:45 +0200

    simulation API inMemoryData

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { PokemonsModule } from './pokemons/pokemons.module';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 @NgModule({
   imports:      [ BrowserModule, 
+                  HttpClientModule,
+                  HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
                   PokemonsModule,
-                  AppRoutingModule 
+                  AppRoutingModule
                 ],
   declarations: [ AppComponent, 
                   PageNotFoundComponent 
